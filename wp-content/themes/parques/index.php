6dfe458fa301fe8657d15af9ef2dded93e08899c
pablopalillo <pablo.576@hotmail.com>
2014-10-17 12:30:38 -0500

    index

--- wp-content/themes/parques/index.php
+++ wp-content/themes/parques/index.php
@@ -4,10 +4,6 @@
 */
 get_header(); 
 ?>
-<header class="page-header">
-	<h1 class="page-title">
-	</h1>
-</header><!-- .page-header -->
 <div id="primary" role="main">
 	<?php /* Slider Home /**/ ?>
 	<?php echo get_new_royalslider(1); ?>
