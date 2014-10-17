e35465fd730d896487cc081e8d73196d4ff61c4c
Emerson Gutierrez <emergugue@gmail.com>
2014-10-17 09:06:43 -0500

    Se adelantaron los estilos del home en menores de 768px

--- wp-content/themes/parques/widgets/widget-categorias.php
+++ wp-content/themes/parques/widgets/widget-categorias.php
@@ -25,7 +25,7 @@ class Widget_Categorias extends WP_Widget
 		extract($args);
 		echo $before_widget;
 		?>
-		<h2>Conoce el proyecto</h2>
+		<h2 class="titulo-principal">Conoce el proyecto</h2>
 		<?php
 		$catCod 	= 3;
 		$categories = getSubcategories($catCod);
@@ -33,11 +33,20 @@ class Widget_Categorias extends WP_Widget
 		<div class="categorias">
 			<?php if( $categories ): ?>
 				<?php foreach ($categories as $key => $category): ?>
-				<div class="categoria">
-					<a id="cat-<?php echo $category->cat_ID ?>" class="<?php echo $category->slug ?> sublink col-sm-4" href="<?php echo get_category_link($category->cat_ID) ?>">
-						<h2><?php echo $category->name ?></h2>
-					</a>
-				</div>
+				<a id="cat-<?php echo $category->cat_ID ?>" class="<?php echo $category->slug ?> sublink col-sm-4" 
+					href="<?php echo get_category_link($category->cat_ID) ?>">
+
+					<article class="subcategory">
+						<header class="subcategory-header">
+							<h2><?php echo $category->name ?></h2>
+						</header>
+						<section> 
+							<div class="subcategory-layer2">
+								<h2><?php echo $category->name ?></h2>
+							</div>
+						</section>
+					</article>
+				</a>
 				<?php endforeach; ?>
 			<?php endif; ?>	
 		</div>	
