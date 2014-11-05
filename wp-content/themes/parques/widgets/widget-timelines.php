e4e0ce44f5b8575d9a967756331a54c529313812
pablopalillo <pablo.576@hotmail.com>
2014-11-05 11:40:24 -0500

    timeline widget twitter

--- wp-content/themes/parques/widgets/widget-timelines.php
+++ wp-content/themes/parques/widgets/widget-timelines.php
@@ -24,12 +24,17 @@ class Widget_Timelines extends WP_Widget
 	{
 		extract($args);
 		echo $before_widget;
+
+
+          
+          
+		
 		?>
 		<div class="timelines">
 			<div class="timeline-twitter">
 				<!-- embebido -->
-			 	<a class="twitter-timeline"  href="https://twitter.com/PabloPalillo" data-widget-id="525733523894718465">Tweets por @PabloPalillo</a>
-            	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+			 	  <a class="twitter-timeline"  href="https://twitter.com/ParqRioMedellin" data-widget-id="530028981425172480">Tweets por @ParqRioMedellin</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 			</div>
 			<div class="facebook">
 			</div>
