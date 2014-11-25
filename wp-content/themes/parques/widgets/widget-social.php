6b9aa010b01aaa32ad2741863218968e7771937f
Emerson Gutierrez <emergugue@gmail.com>
2014-11-25 18:00:08 -0500

    Se creó el botón de Twitter para compartir personalizado, faltan los estilos

--- wp-content/themes/parques/widgets/widget-social.php
+++ wp-content/themes/parques/widgets/widget-social.php
@@ -23,33 +23,36 @@ class Widget_social extends WP_Widget
 	function widgetSocial($args)
 	{
 		extract($args);
+		$url='http://'.$_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
 		echo $before_widget;
 		?>
-		<div class="text">
-			<span> Compartir </span>
+		<div class="linea-color"></div>
+		<div class="contenedor-compartir">
+			<div class="text">
+				<span> Compartir </span>
+			</div>
+
+			<div class="social-buttons row">
+				<div class="googleplus item-social col-sm-4">
+					<div class="share-content">
+						<div class="g-plus" data-action="share" data-annotation="none" data-height="15"></div>
+					</div>
+				</div>
+
+				<div class="facebook item-social col-sm-4">
+					<div class="share-content">
+						<div class="fb-share-button" ></div>
+					</div>
+				</div>
+
+				<div class="twitter item-social col-sm-4">
+					<div class="share-content">
+						<a target="_blank" href="https://twitter.com/intent/tweet?url=<?php echo $url ?>&amp;text=<?php echo wp_title('-', false); ?>&amp;via=ParquesdelRio"><img src="http://logicum.co/wp-content/uploads/2013/01/TwitterTweet.jpg"></a>
+					</div>
+
+				</div>
+			</div>	
 		</div>
-
-		<div class="social-buttons row">
-			<div class="googleplus item-social col-sm-4">
-				<div class="share-content">
-					<div class="g-plus" data-action="share" data-annotation="none" data-height="15"></div>
-				</div>
-			</div>
-
-			<div class="facebook item-social col-sm-4">
-				<div class="share-content">
-					<div class="fb-share-button" ></div>
-				</div>
-			</div>
-
-			<div class="twitter item-social col-sm-4">
-				<div class="share-content">
-					<a href="https://twitter.com/share" class="twitter-share-button" data-via="parqRioMedellin" data-lang="es" data-count="none" data-dnt="true">Twittear</a>
-				</div>
-
-			</div>
-		</div>	
-
 		<?php
 		echo $after_widget;
 	}
